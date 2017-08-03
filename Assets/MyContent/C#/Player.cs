2d8688ad1f5eb9c01f206bbc84be73d702a93bfd
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-03 21:38:25 +0200

    Added very basic movement.

---
+++ Assets/MyContent/C#/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 7;
+
+	Rigidbody rigidbodyThis;
+
+	void Start () 
+	{
+		rigidbodyThis = GetComponent<Rigidbody>();
+	}
+
+	void Update () 
+	{
+		Vector3 movement = transform.forward * (Input.GetAxis("Vertical") * Time.deltaTime * speed);
+		movement += transform.right * (Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+		rigidbodyThis.MovePosition(transform.position + movement);
+	}
+}
