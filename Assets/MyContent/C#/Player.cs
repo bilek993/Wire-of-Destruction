935e5e03b1be840e9e397358aa98323d0a7d5596
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-04 17:32:22 +0200

    Normalized player movement.

--- Assets/MyContent/C#/Player.cs
+++ Assets/MyContent/C#/Player.cs
@@ -15,8 +15,26 @@ public class Player : MonoBehaviour {
 
 	void Update () 
 	{
-		Vector3 movement = transform.forward * (Input.GetAxis("Vertical") * Time.deltaTime * speed);
-		movement += transform.right * (Input.GetAxis("Horizontal") * Time.deltaTime * speed);
-		rigidbodyThis.MovePosition(transform.position + movement);
+		Vector3 movement = transform.forward * (Input.GetAxis("Vertical"));
+		movement += transform.right * (Input.GetAxis("Horizontal"));
+        movement *= speed;
+
+        NormalizeMovement(ref movement);
+
+        movement = transform.position + (movement * Time.deltaTime);
+        rigidbodyThis.MovePosition(movement);
 	}
+
+    void NormalizeMovement(ref Vector3 vectorToNormalize)
+    {
+        float pythagoras = ((vectorToNormalize.x * vectorToNormalize.x) + (vectorToNormalize.z * vectorToNormalize.z));
+
+        if (pythagoras > speed * speed)
+        {
+            float magnitude = Mathf.Sqrt(pythagoras);
+            float multiplier = speed / magnitude;
+            vectorToNormalize.x *= multiplier;
+            vectorToNormalize.z *= multiplier;
+        }
+    }
 }
