4409379b4f54357774794eac4b9b02c3e33e0c97
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-04 19:35:37 +0200

    Added jumping.

--- Assets/MyContent/C#/Player.cs
+++ Assets/MyContent/C#/Player.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float MovementSpeed = 7;
+    public float JumpVelocity = 3;
     public Camera PlayerCamera;
 
-	private Rigidbody _rigidbodyThis;
+	private Rigidbody _playerRigidbody;
+    private bool _canDoubleJump = false;
+    private float _distanceToGround;
 
 	void Start () 
 	{
-		_rigidbodyThis = GetComponent<Rigidbody>();
+		_playerRigidbody = GetComponent<Rigidbody>();
+	    _distanceToGround = GetComponent<CapsuleCollider>().height / 2;
+
         // TODO: Move cursor logic to another class
         MouseHelper.DisableCursor();
 	}
@@ -20,6 +25,7 @@ public class Player : MonoBehaviour {
 	{
         Movement();
         MouseLook();
+        Jumping();
 	}
 
     private void Movement()
@@ -31,13 +37,40 @@ public class Player : MonoBehaviour {
         NormalizeMovement(ref movement);
 
         movement = transform.position + (movement * Time.deltaTime);
-        _rigidbodyThis.MovePosition(movement);
+        _playerRigidbody.MovePosition(movement);
     }
 
     private void MouseLook()
     {
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
         PlayerCamera.transform.Rotate(-Input.GetAxis("Mouse Y"), 0, 0);
+    }
+
+    private void Jumping()
+    {
+        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, _distanceToGround + 0.1f);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (isGrounded)
+            {
+                Jump();
+            }
+            else if (_canDoubleJump)
+            {
+                Jump();
+                _canDoubleJump = false;
+            }
+        }
+
+        if (isGrounded)
+            _canDoubleJump = true;
+    }
+
+    private void Jump()
+    {
+        _playerRigidbody.velocity = Vector3.up * JumpVelocity;
     }
 
     private void NormalizeMovement(ref Vector3 vectorToNormalize)
