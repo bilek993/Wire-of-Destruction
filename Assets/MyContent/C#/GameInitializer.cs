5f939dcb3b9d81947a936a5d3aa0d1ae3ea0a27f
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-08 19:53:06 +0200

    Added dynamic DOF and changed AA method.

---
+++ Assets/MyContent/C#/GameInitializer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class GameInitializer : MonoBehaviour
+{
+    public Player Player;
+    public PostProcessingBehaviour MainPostProcessingBehaviour;
+
+    private DepthOfFieldModel.Settings _settingsDOF;
+    private PostProcessingProfile _postProcessingProfile;
+    private bool _returnToNormalDOF = false;
+    private float _currentFocusDistance;
+
+    void Start()
+    {
+        SetPostProcess();
+		SendMessage("InitializeGame");
+	}
+
+    void Update()
+    {
+        if (_returnToNormalDOF)
+        {
+            if (_currentFocusDistance < 0.47f)
+                _currentFocusDistance += Time.deltaTime * 0.5f;
+
+            updateDOF(_currentFocusDistance);
+        }
+    }
+
+    private void SetPostProcess()
+    {
+        _postProcessingProfile = Instantiate(MainPostProcessingBehaviour.profile);
+        MainPostProcessingBehaviour.profile = _postProcessingProfile;
+        _settingsDOF = _postProcessingProfile.depthOfField.settings;
+        _currentFocusDistance = _settingsDOF.focusDistance;
+    }
+
+    private void updateDOF(float focusDistance)
+    {
+        _settingsDOF.focusDistance = focusDistance;
+        _postProcessingProfile.depthOfField.settings = _settingsDOF;
+    }
+
+    IEnumerator InitializeGame()
+    {
+        yield return new WaitForSeconds(4f);
+        _returnToNormalDOF = true;
+        Player.enabled = true;
+
+        while (true)
+        {
+            if (_currentFocusDistance >= 0.47f)
+                break;
+
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        enabled = false;
+    }
+}
