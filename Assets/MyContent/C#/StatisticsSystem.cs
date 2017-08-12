e0322cd79be54d0c297575923e7b6f1f272c86eb
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-12 20:45:36 +0200

    Added ingame stats.

---
+++ Assets/MyContent/C#/StatisticsSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatisticsSystem : MonoBehaviour
+{
+    public GameObject Canvas;
+    public AudioSource MainAudioSource;
+    public Text FramesText;
+    public Text FramesFixedText;
+
+    private bool _isEnabled;
+	
+	void Update ()
+	{
+	    if (Input.GetKeyUp(KeyCode.BackQuote))
+	    {
+	        _isEnabled = !_isEnabled;
+            Canvas.SetActive(_isEnabled);
+	    }
+
+	    if (_isEnabled)
+	    {
+	        StatFrames();
+	    }
+	}
+
+    void FixedUpdate()
+    {
+        if (_isEnabled)
+        {
+            StatFramesFixed();
+        }
+    }
+
+    private void StatFrames()
+    {
+        FramesText.text = "Timing: " + (1 / Time.smoothDeltaTime).ToString("#0.00") + "FPS (" + Time.smoothDeltaTime.ToString("#0.00") + "ms)";
+    }
+
+    private void StatFramesFixed()
+    {
+        FramesFixedText.text = "Fixed timing: " + (1 / Time.fixedDeltaTime).ToString("#0.00") + "FPS (" + Time.fixedDeltaTime.ToString("#0.00") + "ms)";
+    }
+}
