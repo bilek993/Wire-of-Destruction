ba64c2e35168a7e75359e198878139f20fde385a
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-17 20:58:23 +0200

    Added textures for tube cooler and it's fan.

---
+++ Assets/MyContent/C#/FanRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanRotation : MonoBehaviour
+{
+    public float Speed = 90f;
+
+	void FixedUpdate ()
+    {
+        transform.RotateAround(transform.position, transform.forward, Time.fixedDeltaTime * Speed);
+    }
+}
