c4a5ce6a2dc8ac1d24356a2bb3cb02b0d7b8b76a
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-08-19 14:26:04 +0200

    Added music into the game.

---
+++ Assets/MyContent/C#/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public float MaxAudioVolume = 0.3f;
+    public AudioSource CurrentAudioSource;
+
+	void Start () {
+		
+	}
+
+	void FixedUpdate ()
+    {
+        if (CurrentAudioSource.volume < MaxAudioVolume)
+        {
+            CurrentAudioSource.volume += Time.fixedDeltaTime * 0.05f;
+        }
+	}
+}
