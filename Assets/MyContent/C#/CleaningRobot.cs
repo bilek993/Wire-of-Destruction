d307bc726292e2d0e1604bc408a6cc020c2544cc
DESKTOP-03RQ3MJ\jbilińskipl <jbilinskipl@gmail.com>
2017-09-09 13:35:02 +0200

    Added basic animation for cleaning robot.

---
+++ Assets/MyContent/C#/CleaningRobot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleaningRobot : MonoBehaviour
+{
+    public Transform LightTube;
+
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+		LightTube.RotateAround(LightTube.forward, 0.05f * Time.timeScale);
+	}
+}
